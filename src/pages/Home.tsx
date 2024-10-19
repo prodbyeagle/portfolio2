0c9c25df6896ef6ee02b601182b17457df048e0f
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-10-19 05:13:21 +0200

    updated homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,14 @@ import { projects } from '../data/projects';
 
 const Home: React.FC = () => {
    return (
-      <div className="container mx-auto p-8 flex flex-col md:flex-row">
+      <div className="container mx-auto p-2 lg:p-8 flex flex-col md:flex-row">
          <div className="md:w-1/3">
             <Header />
          </div>
          <div className="md:w-2/3">
+            <div className='text-sm italic'>
+               (Public Only)
+            </div>
             <h2 className="text-3xl font-bold mb-6">Projects</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
                {projects.map((project) => (
