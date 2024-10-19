e35497ec1e34aa81ae0f5fd498bbeb754b6dcc3c
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-10-19 03:20:13 +0200

    config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,21 @@ module.exports = {
     extend: {
       colors: {
         eagle: '#d683ff',
+        html: '#FF5733',
+        css: '#4A90E2',
+        javascript: '#F0DB4F',
+        typescript: '#007ACC',
+        react: '#61DAFB',
+        tailwind: '#38B2AC',
+        python: '#306998',
+        electron: '#5A5B6E',
+        mongodb: '#4DB33D',
+        bird: "#70534c"
       },
       maxWidth: {
-        120: "430px",
-        150: "480px",
-        200: "550px",
+        120: '430px',
+        150: '480px',
+        200: '550px',
       },
     },
   },
