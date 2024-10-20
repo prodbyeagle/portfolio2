d53a5e98cf3f2505a89d773b1936bd823dcb0ebc
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2024-10-21 00:43:41 +0200

    Updated Buttons to be Better :sob:

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -42,49 +42,51 @@ const ProjectCard: React.FC<ProjectProps> = ({ project }) => {
 
    return (
       <div className="relative">
-            <div
-               onClick={openModal}
-               className={`bg-zinc-800/30 p-4 rounded-xl shadow-none hover:shadow-7xl ${project.hoverBGColor} ${project.hoverShadowColor} duration-1000 transition-all border border-zinc-600 cursor-pointer`}
-            >
-               <div className="flex items-center mb-4">
-                  <img
-                     src={project.imageUrl}
-                     alt={project.title}
-                     className="w-6 h-6 object-cover mr-2 rounded-md"
-                     loading="lazy"
-                  />
-                  <h3 className="text-xl font-medium mt-0">{project.title}</h3>
-               </div>
-               <p className="mt-2">{project.description}</p>
-               <div className="flex flex-wrap mt-4">
-                  {project.tags.map((tag) => (
-                     <span
-                        key={tag}
-                        className={`${tagColors[tag] || 'bg-gray-600'} text-white text-xs font-semibold px-2 py-1 rounded-full mr-2`}
-                     >
-                        {tag}
-                     </span>
-                  ))}
-               </div>
+         <div
+            onClick={openModal}
+            className={`bg-zinc-800/30 p-4 rounded-xl shadow-none hover:shadow-7xl ${project.hoverBGColor} ${project.hoverShadowColor} duration-1000 transition-all border border-zinc-600 cursor-pointer`}
+         >
+            <div className="flex items-center mb-4">
+               <img
+                  src={project.imageUrl}
+                  alt={project.title}
+                  className="w-6 h-6 object-cover mr-2 rounded-md"
+                  loading="lazy"
+               />
+               <h3 className="text-xl font-medium mt-0">{project.title}</h3>
             </div>
+            <p className="mt-2">{project.description}</p>
+            <div className="flex flex-wrap mt-4">
+               {project.tags.map((tag) => (
+                  <span
+                     key={tag}
+                     className={`${tagColors[tag] || 'bg-gray-600'} text-white text-xs font-semibold px-2 py-1 rounded-full mr-2`}
+                  >
+                     {tag}
+                  </span>
+               ))}
+            </div>
+         </div>
 
          <Modal isOpen={isModalOpen} onClose={closeModal} title={`${project.title} - ${project.year}`}>
             <div className="flex flex-col items-center">
                <img
                   src={project.imageUrl}
                   alt={project.title}
-                  className="w-32 h-32 object-cover mb-4 rounded-lg"
+                  className="w-32 h-32 object-cover mb-4 rounded-xl"
                   loading="lazy"
                />
                <p className="text-lg text-white text-center mb-4 break-words">
                   {project.extendedDescription}
                </p>
+            </div>
+            <div className="flex justify-center mt-4 space-x-2">
                {project.repoLink && (
                   <a
                      href={project.repoLink}
                      target="_blank"
                      rel="noopener noreferrer"
-                     className="text-blue-500 hover:underline"
+                     className="bg-zinc-800 text-white px-4 py-2 rounded-lg shadow-md hover:bg-zinc-700 border border-zinc-600 transition-all duration-100"
                   >
                      View Repository
                   </a>
@@ -94,7 +96,7 @@ const ProjectCard: React.FC<ProjectProps> = ({ project }) => {
                      href={project.homepageLink}
                      target="_blank"
                      rel="noopener noreferrer"
-                     className="text-blue-500 hover:underline"
+                     className="bg-zinc-800 text-white px-4 py-2 rounded-lg shadow-md hover:bg-zinc-700 border border-zinc-600 transition-all duration-100"
                   >
                      View Website
                   </a>
