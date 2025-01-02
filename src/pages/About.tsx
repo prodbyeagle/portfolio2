0526641493acdad97a41e7773bf92fc1f224e24f
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-01-02 14:55:59 +0100

    updated ui and updated links

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { GithubLogo, TwitterLogo, InstagramLogo, DiscordLogo, ArrowLeft } from '@phosphor-icons/react';
+import { GithubLogo, XLogo, InstagramLogo, DiscordLogo, ArrowLeft, ThreadsLogo } from '@phosphor-icons/react';
 
 export const About: React.FC = () => {
    return (
       <div className="min-h-screen bg-neutral-900 text-white px-6 lg:px-12 py-16">
-         <div className="max-w-4xl mx-auto">
+         <div className="max-w-4xl mx-auto mt-16">
             <div className="text-center">
                <h1 className="text-4xl font-bold">About Me</h1>
                <p className="text-lg text-neutral-400 mt-2">Here’s a little glimpse into my world!</p>
@@ -13,9 +13,8 @@ export const About: React.FC = () => {
 
             <div className="mt-12 space-y-12">
                <section>
-                  <h2 className="text-3xl font-semibold text-neutral-100">Who Am I?</h2>
                   <p className="text-lg text-neutral-400 mt-4 leading-relaxed">
-                     <span className="text-4xl font-bold text-eagle">Hi there!</span> I’m Noah, but you might know me better as{' '}
+                     <span className="text-4xl font-bold text-neutral-100">Hi there!</span> I’m Noah, but you might know me better as{' '}
                      <span className="eagle-text font-semibold">@prodbyeagle</span>. I’m a dedicated Frontend Developer based in Nuremberg, Germany.
                      My journey into web development began out of curiosity, but it quickly grew into a deep passion that I pursue wholeheartedly every day.
                   </p>
@@ -25,7 +24,7 @@ export const About: React.FC = () => {
                   <h2 className="text-3xl font-semibold text-neutral-100">My Journey in Tech</h2>
                   <div className="mt-4 space-y-4">
                      <p className="text-lg text-neutral-400 leading-relaxed">
-                        My coding journey began with Python, sparking a fascination for problem-solving and technology. Since then, I’ve delved into tools and technologies like React, JavaScript, TypeScript, and Tailwind CSS. My focus is on creating sleek, responsive websites with intuitive user experiences.
+                        My coding journey began with Python, sparking a fascination for problem-solving and technology. Since then, I’ve delved into tools and technologies like Next, TypeScript, and Tailwind. My focus is on creating sleek, minimalistic and responsive websites with intuitive user experiences.
                      </p>
                      <p className="text-lg text-neutral-400 leading-relaxed">
                         I thrive on exploring front-end frameworks and experimenting with modern libraries, always striving to push the limits of what’s possible in web development.
@@ -44,27 +43,30 @@ export const About: React.FC = () => {
                <section className="text-center">
                   <h2 className="text-3xl font-semibold text-neutral-100">Let’s Connect</h2>
                   <p className="text-lg text-neutral-400 mt-4">
-                     Want to connect or see more of my work? Check out my social links below! I’m always open to collaborating and exploring exciting new ideas.
+                     I’m not very active on social media, but feel free to connect through the links below! I’m always open to collaborating and exploring exciting new ideas.
                   </p>
                   <div className="flex justify-center space-x-6 mt-6">
                      <a href="https://github.com/prodbyeagle" target="_blank" rel="noopener noreferrer">
-                        <GithubLogo className="text-3xl text-neutral-400 hover:text-eagle transition" />
+                        <GithubLogo className="text-3xl text-neutral-400 hover:text-white hover:scale-110 transition duration-300" />
                      </a>
-                     <a href="https://twitter.com/prodbyeagle" target="_blank" rel="noopener noreferrer">
-                        <TwitterLogo className="text-3xl text-neutral-400 hover:text-eagle transition" />
+                     <a href="https://threads.net/@prodbyeagle" target="_blank" rel="noopener noreferrer">
+                        <ThreadsLogo className="text-3xl text-neutral-400 hover:text-white hover:scale-110 transition duration-300" />
+                     </a>
+                     <a href="https://x.com/prodbyeagle" target="_blank" rel="noopener noreferrer">
+                        <XLogo className="text-3xl text-neutral-400 hover:text-white hover:scale-110 transition duration-300" />
                      </a>
                      <a href="https://instagram.com/prodbyeagle" target="_blank" rel="noopener noreferrer">
-                        <InstagramLogo className="text-3xl text-neutral-400 hover:text-eagle transition" />
+                        <InstagramLogo className="text-3xl text-neutral-400 hover:text-white hover:scale-110 transition-all duration-300" />
                      </a>
                      <a href="https://discord.gg/V33nExqB68" target="_blank" rel="noopener noreferrer">
-                        <DiscordLogo className="text-3xl text-neutral-400 hover:text-eagle transition" />
+                        <DiscordLogo className="text-3xl text-neutral-400 hover:text-white hover:scale-110 transition-all duration-300" />
                      </a>
                   </div>
                </section>
             </div>
 
             <div className="mt-16 text-center">
-               <Link to="/" className="text-eagle hover:underline flex items-center justify-center gap-2">
+               <Link to="/" className="text-neutral-100 hover:underline flex items-center justify-center gap-2">
                   <ArrowLeft /> Back to Home
                </Link>
             </div>
